a798694fddff32be1b13f1088d8f618cda4b3886
barleypeg <barleypeg@urbandaleschools.com>
2019-10-01 14:24:04 -0500

    chapter three

--- 4.2_Grading_2.0.py
+++ 4.2_Grading_2.0.py
@@ -4,3 +4,16 @@ GRADING 2.0
 Copy your Grading 1.0 program and modify it to also print out the letter grade depending on the numerical grade.
 If they fail, tell them to "Transfer to Johnston!"
 '''
+
+print()
+print("Welcome to Peggy's Grade Calculator")
+print()
+sg = float(input("What is your semester grade?"))
+fe = float(input("What is your final exam grade?"))
+ew = float(input("What is your exam worth?"))
+ew = ew/100
+grade = sg*(1-ew) + fe*(ew)
+print()
+
+if grade
+print("Your final grade is:", grade)
