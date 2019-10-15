c22f561476993728fcceb3c085f2a0c753b8389a
barleypeg <barleypeg@urbandaleschools.com>
2019-10-15 14:23:37 -0500

    chapter four

--- 4.3_Quiz_Master.py
+++ 4.3_Quiz_Master.py
@@ -10,15 +10,29 @@ print()
 print("Welcome to Peggy's Riddle Quiz!")
 print()
 print()
-q1 = str(input("What is it that no man ever did see, which never was, but always is to be? \n A. The Future \n B. Tomorrow \n C. Death \n D. Time "))
 
-if q1.lower == "a" or "the future" or "c" or "death" or "d" or "time":
-  print("Incorrect! The right answer is B. Tomorrow")
-elif q1.lower == "tomorrow" or "b":
+q1 = input("What is it that no man ever did see, which never was, but always is to be? \n A. The Future \n B. Tomorrow \n C. Death \n D. Time ")
+if q1.lower() == "tomorrow" or q1.lower() == "b":
     print("Correct!")
     score += 1
 else:
-    print("Incorrect! The right answer is; B. Tomorrow")
+    print("Incorrect! The right answer is: B. Tomorrow")
 
+
+q2 = input("What goes through a door but never goes in and never comes out? \n A. Keys \n B. A Doorknob \n C. A Keyhole \n D. Air, It's Everywhere ")
 print(score)
+if q2.lower() == "a keyhole" or q2.lower() == "c":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect! The right answer is: C. A Keyhole")
+
+
+q3 = input("What goes through a door but never goes in and never comes out? \n A. Keys \n B. A Doorknob \n C. A Keyhole \n D. Air, It's Everywhere ")
+print(score)
+if q3.lower() == "a keyhole" or q3.lower() == "c":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect! The right answer is: C. A Keyhole")
 
