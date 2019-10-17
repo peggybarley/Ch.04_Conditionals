f8a375b29849f382f106ff9350cb746aa29743f9
barleypeg <barleypeg@urbandaleschools.com>
2019-10-17 14:14:23 -0500

    chapter four

--- 4.3_Quiz_Master.py
+++ 4.3_Quiz_Master.py
@@ -11,28 +11,27 @@ print("Welcome to Peggy's Riddle Quiz!")
 print()
 print()
 
-q1 = input("What is it that no man ever did see, which never was, but always is to be? \n A. The Future \n B. Tomorrow \n C. Death \n D. Time ")
-if q1.lower() == "tomorrow" or q1.lower() == "b":
+q1 = input("What goes through a door but never goes in and never comes out? \n A. Keys \n B. A Doorknob \n C. Air, It's Everywhere \n D. A Keyhole \n  ")
+if q1.lower() == "a keyhole" or q1.lower() == "d":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect! The right answer is: D. A Keyhole")
+print()
+
+q2 = input("What is it that no man ever did see, which never was, but always is to be? \n A. The Future \n B. Tomorrow \n C. Death \n D. Time \n   ")
+if q2.lower() == "tomorrow" or q2.lower() == "b":
     print("Correct!")
     score += 1
 else:
     print("Incorrect! The right answer is: B. Tomorrow")
+print()
 
-
-q2 = input("What goes through a door but never goes in and never comes out? \n A. Keys \n B. A Doorknob \n C. A Keyhole \n D. Air, It's Everywhere ")
-print(score)
-if q2.lower() == "a keyhole" or q2.lower() == "c":
+q3 = input("? \n A. \n B. \n C. \n D. \n  ")
+print()
+if q3.lower() == " " or q3.lower() == " ":
     print("Correct!")
     score += 1
 else:
-    print("Incorrect! The right answer is: C. A Keyhole")
-
-
-q3 = input("What goes through a door but never goes in and never comes out? \n A. Keys \n B. A Doorknob \n C. A Keyhole \n D. Air, It's Everywhere ")
-print(score)
-if q3.lower() == "a keyhole" or q3.lower() == "c":
-    print("Correct!")
-    score += 1
-else:
-    print("Incorrect! The right answer is: C. A Keyhole")
-
+    print("Incorrect! The right answer is: ")
+print()
